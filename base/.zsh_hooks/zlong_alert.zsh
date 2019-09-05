60412d7a3b1f20e5458e44412d467dcb17c6a0ef
kevin lui <kevinywlui@gmail.com>
2019-09-05 13:00:16 -0700

    Fix style of zlong

--- base/.zsh_hooks/zlong_alert.zsh
+++ base/.zsh_hooks/zlong_alert.zsh
@@ -2,7 +2,7 @@
 zlong_ignore_cmds='vim ssh'
 
 # Define what a long duration is
-zlong_duration=15
+zlong_duration=2
 
 # Need to set an initial timestamps otherwise, we'll be comparing an empty
 # string with an integer.
@@ -26,9 +26,14 @@ zlong_alert_post() {
     local duration=$(($EPOCHSECONDS - $zlong_timestamp))
     local lasted_long=$(($duration - $zlong_duration))
     local cmd_head=$(echo $zlong_last_cmd | cut -d ' ' -f 1)
-    if [[ $lasted_long -gt 0 && ! -z $zlong_last_cmd && ! *"$cmd_head"*==$zlong_ignore_cmds ]]; then
+    if [[ $lasted_long -gt 0 && \
+        ! -z $zlong_last_cmd && \
+        ! $zlong_last_cmd =~ $cmd_head ]] 
+    then
         zlong_alert_func $zlong_last_cmd duration
     fi
+    echo $cmd_head
+    echo $zlong_ignore_cmds
     zlong_last_cmd=''
 }
 
