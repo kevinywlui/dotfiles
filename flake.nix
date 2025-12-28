253c333a8ced8a7fd0a1b2eb1ebd5b53e197c883
Kevin Lui <kevinywlui@gmail.com>
2025-12-27 22:32:30 -0800

    refactor: remove VM configuration and simplify flake.nix

--- flake.nix
+++ flake.nix
@@ -10,57 +10,31 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, disko, home-manager, nixos-hardware, ... }@inputs:
-    let
+  outputs = { nixpkgs, nixos-hardware, disko, home-manager, ... }@inputs: {
+    nixosConfigurations.fw13 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      
-      unstableOverlay = final: prev: {
-        unstable = import inputs.nixpkgs-unstable {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      };
-
-      commonModules = [
+      specialArgs = { inherit inputs; };
+      modules = [
+        nixos-hardware.nixosModules.framework-intel-core-ultra-series1
+        disko.nixosModules.disko
         home-manager.nixosModules.home-manager
+        ./disko.nix
+        ./hardware-configuration.nix
+        ./configuration.nix
         {
-          nixpkgs.overlays = [ unstableOverlay ];
+          nixpkgs.overlays = [
+            (final: prev: {
+              unstable = import inputs.nixpkgs-unstable {
+                system = final.system;
+                config.allowUnfree = true;
+              };
+            })
+          ];
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.klui = import ./home.nix;
         }
       ];
-    in
-    {
-    nixosConfigurations.fw13 = nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = { inherit inputs; };
-      modules = [
-        nixos-hardware.nixosModules.framework-intel-core-ultra-series1
-        disko.nixosModules.disko
-        ./disko.nix
-        ./hardware-configuration.nix
-        ./configuration.nix
-      ] ++ commonModules;
-    };
-
-    nixosConfigurations.my-test-vm = nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./configuration.nix
-        ({ pkgs, ... }: {
-          # Minimal configuration for VM
-          boot.loader.systemd-boot.enable = true;
-          boot.loader.grub.enable = false;
-          fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
-        })
-      ] ++ commonModules;
-    };
-
-    apps.x86_64-linux.default = {
-      type = "app";
-      program = "${self.nixosConfigurations.my-test-vm.config.system.build.vm}/bin/run-fw13-vm";
     };
   };
 }
