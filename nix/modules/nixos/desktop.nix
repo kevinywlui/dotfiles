ef555b837c52333784860abd1fb56b564e62951b
Kevin Lui <kevinywlui@gmail.com>
2026-01-06 23:33:17 -0800

    nixos/desktop: remove manual CPU perf limits to respect EPP/governor

--- nix/modules/nixos/desktop.nix
+++ nix/modules/nixos/desktop.nix
@@ -99,11 +99,6 @@
 
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
     };
   };
 }
