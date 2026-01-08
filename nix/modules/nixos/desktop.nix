7a4c3ae509c87f149dd695d49132522dcd3e0c57
Kevin Lui <kevinywlui@gmail.com>
2026-01-07 21:00:02 -0800

    nixos/desktop: optimize TLP for Intel Core Ultra (Meteor Lake)
    Updates TLP settings to use 'powersave' governor with specific EPP hints ('balance_performance'/'balance_power') and platform profiles. This configuration is recommended for modern Intel processors to effectively balance performance and battery life.

--- nix/modules/nixos/desktop.nix
+++ nix/modules/nixos/desktop.nix
@@ -109,11 +109,14 @@
   services.tlp = {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
     };
   };
 }
